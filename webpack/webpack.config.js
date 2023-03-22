38511dc705312d7293a32410dd24b5cca241b251
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2023-03-21 23:15:06 -0400

    set up empty tyepscript chrome extension

---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = path.join(__dirname, "..", "src");
+
+module.exports = {
+   mode: "production",
+   entry: {
+    //popup: path.join(srcDir, 'popup.tsx'),
+    //options: path.join(srcDir, 'options.tsx'),
+    background: path.join(srcDir, 'background.ts'),
+    content: path.join(srcDir, 'content.ts'),
+   },
+   output: {
+      path: path.join(__dirname, "../dist"),
+      filename: "[name].js",
+   },
+   resolve: {
+      extensions: [".ts", ".js"],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+         },
+      ],
+   },
+   plugins: [
+    new CopyPlugin({
+        patterns: [{ from: ".", to: ".", context: "public" }],
+        options: {},
+    }),
+   ],
+};
