de49dfba0df9ac483415fa134d856e19ce5c6225
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2023-04-30 00:56:27 -0400

    get a barebones popup with vue

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 .PHONY: zip
 zip:
-	if [ -d "load_me" ]; then zip -r dist.zip load_me; else echo "run 'npm run build'"; fi;
+	if [ -d "dist" ]; then zip -r dist.zip dist; else echo "run 'npm run build'"; fi;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { VueLoaderPlugin } = require('vue-loader');
 const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
    mode: "production",
    entry: {
-      //popup: path.join(srcDir, 'popup.tsx'),
+      'popup/index': path.join(srcDir, 'popup/main.ts'),
       //options: path.join(srcDir, 'options.tsx'),
       background: [
          path.join(srcDir, 'background.ts'),
@@ -19,14 +21,32 @@ module.exports = {
       filename: "[name].js",
    },
    resolve: {
-      extensions: [".ts", ".js"],
+      extensions: [".ts", ".js", ".vue"],
+      alias: {
+         "@/*": [
+            "./src/*"
+         ],
+         vue: '@vue/runtime-dom'
+      },
+      modules: ['node_modules']
    },
    module: {
       rules: [
          {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
+            test: /\.ts$/,
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/]
+            },
+            exclude: /node_modules/
+          },
+         {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+         },
+         {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
          },
       ],
    },
@@ -34,6 +54,10 @@ module.exports = {
     new CopyPlugin({
         patterns: [{ from: ".", to: ".", context: "public" }],
         options: {},
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
     }),
    ],
 };
